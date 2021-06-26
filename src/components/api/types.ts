ebd21bb03afe0947a03b812f5abe0521be1dbda5
laxus23 <laxus2323@o2.pl>
2021-06-26 12:21:39 +0200

    Create types.ts

---
+++ src/components/api/types.ts
@@ -0,0 +1,57 @@
+export interface User {
+  id: number;
+  name: string;
+  company: {
+    name: string;
+    bs: string
+  }
+}
+
+export interface Post {
+  id: number,
+  title: string,
+  body: string,
+  userId: number,
+  userData?: User
+}
+export interface Tag {
+  id: number,
+  name: string,
+  category: string
+}
+
+export interface Entity {
+  name: string,
+  entity: string,
+  location: string,
+  expertise: string,
+  date: string,
+  firm: string
+}
+
+export interface Photo {
+  albumId: number,
+  id: number,
+  title: string,
+  url: string,
+  thumbnailUrl: string
+}
+
+export interface Comment {
+  postId: number,
+  id: number,
+  name: string,
+  email: string,
+  body: string
+}
+export interface EntityType {
+  title: string,
+  body: string
+}
+export interface Fee {
+  year: number,
+  costCenter: string,
+  totalAmount: string,
+  lawFirm: string
+}
+
